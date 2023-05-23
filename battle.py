fa64686007ec20cf9a4cd30932cc01c7c200033c
Andrew Wright <awright@navisite.com>
2023-05-23 02:29:55 -0400

    Roughest possible battle framework

--- battle.py
+++ battle.py
@@ -1,5 +1,40 @@
+import random
 
 import factory
+import dice
+
+class Team:
+
+    def __init__(self, name):
+        self.name = name
+        self.members = []
+
+    def add(self, c):
+        self.members.append(c)
+
+    def randomMember(self, alive=None):
+
+        options = self.members
+
+
+        c1 = len(options)
+        if alive:
+            for member in options:
+                if not member.canAct():
+                    options.remove(member)
+
+        c2 = len(options)
+        print(' member filtering - {} - {}'.format(c1, c2))
+
+        # TODO: should this raise an error or something if the filters remove all possible options?
+        return random.choice(options)
+
+    def remaining(self):
+        # count of remaining active fighters
+        a = len([x for x in self.members if x.canAct()])
+        print('R: {}'.format(a))
+        return a
+
 
 class Battle:
 
@@ -19,9 +54,9 @@ class Battle:
         print()
 
         count = 1
-        for team, members in self.teams.items():
-            print('Team #{}: {}'.format(count, team))
-            for fellah in members:
+        for name, team in self.teams.items():
+            print('Team #{}: {}'.format(count, name))
+            for fellah in team.members:
                 print('    {}'.format(str(fellah)))
             print()
             count += 1
@@ -34,36 +69,91 @@ class Battle:
         while (self.state == Battle.BATTAL):
             print('ROUND {}'.format(round_count))
 
+            # determine turn order
             self.resetInitiative()
 
+
+            # everybody gets a turn
+            while len(self.initiative) > 0:
+                current = self.initiative.pop()
+                # remember its a tuple
+                fellah = current[1]
+
+
+                if fellah.canAct():
+
+                    # determine a course of action
+
+                    action = fellah.getCombatAction(self)
+
+
+                    # execute it
+
+                    self.processAction(fellah, action)
+                else:
+                    print('Skipping {}, they are having a rough day'.format(fellah.name))
+
+
+                team_counts = [t.remaining() for t in self.teams.values()]
+                print('Counts: {}'.format(team_counts))
+                if 0 in team_counts:
+                    self.state = Battle.OVER
+                    break
+
+            round_count += 1
+
+        print('All done')
+
+
+        # TODO: I think the isAlive filtering in random member is destroying the member lists
+        print([len(x.members) for x in self.teams.values()])
+
+
+            
+    def processAction(self, actor, action):
+        # for now we'll just do basic ass dex checks
+        target = action['target']
+        success = actor.rollStat('dex')
+        if success:
+
+            damage = dice.Dice.d(1,6)
+            target.applyDamage(damage)
+
+            descriptor = '{} attacked {} with a sword or something, dealing {} damage. They are now {}'.format(actor.name, target.name, damage, target.status())
+        else:
+            descriptor = '{} attacked {} with a sword or something, but missed'.format(actor.name, target.name,)
+
+        print(descriptor)
 
 
     def resetInitiative(self):
         self.initiative = []
 
-        for team, members in self.teams.items():
-            for fellah in members:
+        for name, team in self.teams.items():
+            for fellah in team.members:
 
-                x = fellah.generateInitiative()
+                if fellah.canAct():
+                    x = fellah.generateInitiative()
 
-                self.initiative.append( (x, fellah) )
+                    self.initiative.append( (x, fellah) )
 
-        print(len(self.initiative))
-        print(self.initiative)
         self.initiative.sort(key=lambda x: x[0])
-        print()
-        print(self.initiative)
 
 
     def addParticipant(self, side, creature):
+        # TODO: handle state conditions
         if not self.teams.get(side, False):
-            self.teams[side] = []
+            self.teams[side] = Team(side)
 
-        self.teams[side].append(creature)
+        self.teams[side].add(creature)
 
+    def randomTeam(self, exclude=None):
+        team_options = list(self.teams.keys())
 
+        if exclude:
+            team_options.remove(exclude)
 
-
+        return self.teams[random.choice(team_options)]
 
 
 if __name__ == "__main__":
--- dungeons.py
+++ dungeons.py
@@ -67,3 +67,39 @@ class Creature:
 
     def __str__(self):
         return self.name + ', ' + self.stock + ' ' + self.job + '; S: ' + str(self.str) + ', D: ' + str(self.dex) + ', C: ' + str(self.con) + ', I: ' + str(self.int) + ', W: ' + str(self.wis) + ', C: ' + str(self.cha)
+
+    def canAct(self):
+        return self.currenthp > 0
+
+    def getCombatAction(self, battle):
+        # given the current battle state 
+
+        target_team = battle.randomTeam(exclude=self.type) # using self.type won't work in the long run
+
+        x = {
+            'action_type': 'melee attack',
+            'target': target_team.randomMember(alive=True)
+        }
+        # print(' Me: {}, {}'.format(self.name, self.type))
+        # print('Him: {}, {}'.format(x['target'].name, x['target'].type))
+        return x
+
+    def applyDamage(self, damage_count):
+        old = self.currenthp
+        self.currenthp = self.currenthp - damage_count
+        if self.currenthp < 0:
+            self.currenthp = 0
+        print('  HP old - {}, new - {}'.format(old, self.currenthp))
+
+    def status(self):
+        if self.currenthp == 0:
+            return 'dead'
+        elif self.currenthp < self.currenthp / 2:
+            return 'injured'
+        else:
+            return 'mostly ok'
+
+    def rollStat(self, stat):
+        target = getattr(self, stat)
+        return dice.Dice.d(1,20) <= target
+        
